04cc632df2c3afb08dd0a6504fb36b80050961e1
Dylan Meysmans <contact@mettekou.com>
2022-02-09 18:53:25 +0100

    Mirror type provider SDK example

--- FSharp.Data.Tdms/FSharp.Data.Tdms.Runtime.fs
+++ FSharp.Data.Tdms/FSharp.Data.Tdms.Runtime.fs
@@ -20,3 +20,9 @@ module Helpers =
 
         let concrete = generic.MakeGenericMethod [| ty |]
         concrete.Invoke(null, [| group; channel; index |])
+
+#if !IS_DESIGNTIME
+// Put the TypeProviderAssemblyAttribute in the runtime DLL, pointing to the design-time DLL
+[<assembly:CompilerServices.TypeProviderAssembly("FSharp.Data.Tdms.DesignTime.dll")>]
+do ()
+#endif
