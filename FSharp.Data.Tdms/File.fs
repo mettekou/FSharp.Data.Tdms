a438500d46d9da054d4d008401523720916f85dc
Dylan Meysmans <contact@mettekou.com>
2022-02-22 23:20:09 +0100

    Add type signature

--- FSharp.Data.Tdms/File.fs
+++ FSharp.Data.Tdms/File.fs
@@ -131,7 +131,7 @@ module File =
         ofObjects path objects
 
 
-    let readAsyncCt ct path writeIndex =
+    let readAsyncCt (ct: CancellationToken) (path: string) (writeIndex: bool) : File Task =
         backgroundTask {
             let indexPath =
                 Path.ChangeExtension(path, ".tdms_index")
