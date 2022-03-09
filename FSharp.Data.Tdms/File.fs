4bc2d689abe427ec55da5bb46148db9e28a82327
Dylan Meysmans <contact@mettekou.com>
2022-03-09 23:49:57 +0100

    Remove background task where it breaks type checking

--- FSharp.Data.Tdms/File.fs
+++ FSharp.Data.Tdms/File.fs
@@ -131,8 +131,8 @@ module File =
         ofObjects path objects
 
 
-    let readAsyncCt ct path writeIndex : File Task =
-        backgroundTask {
+    let readAsyncCt ct path writeIndex =
+        task {
             let indexPath =
                 Path.ChangeExtension(path, ".tdms_index")
 
